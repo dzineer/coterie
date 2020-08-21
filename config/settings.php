d0e9979da887a0f23f35eddea2c9f4e98d9ea5cb
S.B. Longchamp <sarah@longchamp.codes>
2020-08-21 19:30:08 -0400

    Removed thing

--- config/settings.php
+++ config/settings.php
@@ -6,20 +6,6 @@ use DI\ContainerBuilder;
 return function (ContainerBuilder $containerBuilder) {
     // Global Settings Object
     $containerBuilder->addDefinitions([
-        LoggerInterface::class => function (ContainerInterface $c) {
-            $settings = $c->get('settings');
-
-            $loggerSettings = $settings['logger'];
-            $logger = new Logger($loggerSettings['name']);
-
-            $processor = new UidProcessor();
-            $logger->pushProcessor($processor);
-
-            $handler = new StreamHandler($loggerSettings['path'], $loggerSettings['level']);
-            $logger->pushHandler($handler);
-
-            return $logger;
-        },
         'settings' => array(
             'displayErrorDetails' => true,
             'logger' => [
